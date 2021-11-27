d42bde0abd23334f5918944fd385a0f5fd44c509
aneud <aneud@DESKTOP-GQNJOI0>
2021-11-26 20:12:06 -0400

    Agregar archivos de proyecto.

---
+++ CSrepro/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace CSrepro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listSong = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(500, 370);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // listSong
+            // 
+            this.listSong.FormattingEnabled = true;
+            this.listSong.Location = new System.Drawing.Point(529, 13);
+            this.listSong.Name = "listSong";
+            this.listSong.Size = new System.Drawing.Size(188, 316);
+            this.listSong.TabIndex = 1;
+            this.listSong.DoubleClick += new System.EventHandler(this.listSong_DoubleClick);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(529, 338);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(91, 44);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(626, 338);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(91, 44);
+            this.btnRemover.TabIndex = 2;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 394);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.listSong);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox listSong;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnRemover;
+    }
+}
+
---
+++ CSrepro/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSrepro
+{
+    public partial class Form1 : Form
+    {
+        List<string> songNames;
+        List<string> songPaths;    
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+
+            if (dialog.ShowDialog()== DialogResult.OK)
+            {
+                AddsongTolist( dialog.SafeFileNames.ToList(), dialog.FileNames.ToList());
+               
+            }
+            
+
+        }
+       
+        private void AddsongTolist(List<string> names, List<string> paths)
+        {
+            if (songNames == null)
+                songNames = new List<string>(); 
+            
+            if (songPaths==null)
+                songPaths = new List<string>();
+
+            foreach (var item in names)
+            {
+                if (!ExistonList(item))
+                {
+                    songNames.Add(item);
+                    songPaths.Add(GetPath(item, paths));
+                }
+
+            }
+
+            listSong.DataSource  = songNames;
+        }
+
+        private bool ExistonList(string song)
+        {
+            bool exist = false;
+            foreach (var item in songNames)
+            {
+                
+                if (item == song)
+               
+                    exist = true;
+                   
+            }
+            return exist;
+        }
+
+        private string  GetPath(string filename, List <string> songsPath = null)
+        {
+            string actualPath = string.Empty;
+            if (songsPath == null)
+               songsPath = songPaths;
+
+
+            foreach (var path in songsPath)
+            {
+                if (path.Contains(filename))
+
+                    actualPath = path;                               
+            }
+            return actualPath;
+        }       
+                
+ 
+        private void listSong_DoubleClick(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = GetPath(listSong.Text);
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if(songNames=!null)
+            songNames.Remove(listSong.Text);
+            if (songPaths = !null)
+                songPaths.Remove(listSong.Text);
+        }
+        private void RefreshList()
+        {
+
+        }
+
+    }
+
+       
+
+}
